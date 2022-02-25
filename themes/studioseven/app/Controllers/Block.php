93de4f36f343c9a2276a5b4526487f504b46abad
Matth-Ben <matthias.benoit@outlook.fr>
2022-02-25 23:46:13 +0100

    🎆 [THEMES] Add themes

---
+++ themes/studioseven/app/Controllers/Block.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+use Sober\Controller\Controller;
+
+class Block extends Controller {
+    // public static function example($data) {
+    //     return [
+    //         'title' => Element::title($data),
+    //         'image' => Element::image($data['image'], '1920px')
+    //     ];
+    // }
+
+    // generated function here
+}
